63a3b77914bddc1eb20bf0f9efbbec7bb9031af4
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2022-01-08 09:54:08 -0500

    updated

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="insurance-prediction",
     version="0.0.3",
     description="insurance-prediction",
-    author="Avnish yadav",
     packages=find_packages(),
     license="MIT"
 )
